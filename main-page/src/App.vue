1ea12c9f55628b5201e06e854732956d0cce3080
Eugene Malin(Novikov) <jenia96@live.com>
2020-03-17 00:27:03 +0300

    Remove demo components and add base template for head part of task

--- main-page/src/App.vue
+++ main-page/src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+const navigationItems = [
+  {
+    id: 'about',
+    checked: false,
+    marker: null,
+    title: 'О школе'
+  },
+  {
+    id: 'courses',
+    checked: false,
+    marker: '1 бесплатный',
+    title: 'Курсы'
+  },
+  {
+    id: 'teachers',
+    checked: false,
+    marker: null,
+    title: 'Преподаватели'
+  },
+  {
+    id: 'forYou',
+    checked: false,
+    marker: null,
+    title: 'Для вас'
+  }
+]
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data: {
+    navigationItems
   }
 }
 </script>
