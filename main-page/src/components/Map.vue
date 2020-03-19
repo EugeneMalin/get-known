7cc66440c37a607a5c6ca5bec25c22ad3af12df3
Eugene Malin(Novikov) <jenia96@live.com>
2020-03-19 23:26:23 +0300

    Add map

---
+++ main-page/src/components/Map.vue
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+<template>
+  <div class="map">
+    <img class="map-image" src="@/assets/world.png"/>
+    <div class="map-title"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        points: Array
+    }
+}
+</script>
+
+<style lang="less">
+    .map {
+        position: relative;
+        display: flex;
+        width: 100%;
+        height: 100%;
+        &-image {
+            width: 100%;
+            height: 100%;
+        }
+    }
+</style>
